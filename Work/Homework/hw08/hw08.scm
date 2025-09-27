081bdb52484bcaca475c9d3fe84e5ae8f2f08fa9
Linox <linox1953@outlook.com>
2025-09-27 17:34:05 +0800

    hw08

---
+++ Work/Homework/hw08/hw08.scm
@@ -0,0 +1,34 @@
+(define (ascending? s) 
+    (if (or (null? s) (null? (cdr s)))
+        #t
+        (if (> (car s) (car (cdr s)))
+            #f
+            (ascending? (cdr s))
+        )
+    )
+)
+
+
+
+(define (my-filter pred s) 
+    (if (null? s)
+        nil
+        (if (pred (car s))
+            (append (list (car s)) (my-filter pred (cdr s)))
+            (my-filter pred (cdr s))
+        )
+    )
+)
+
+(define (interleave lst1 lst2) 
+    (cond 
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (append (list (car lst1)) (interleave lst2 (cdr lst1))))
+    )
+)
+
+(define (no-repeats s)
+  (if (null? s) s
+    (cons (car s)
+      (no-repeats (filter (lambda (x) (not (= (car s) x))) (cdr s))))))
