9e02d7abac22d7c899b9a68765eafb04eaa27104
Linox <linox1953@outlook.com>
2025-10-09 18:47:14 +0800

    lab11

--- Work/Lab/lab11/lab11.scm
+++ Work/Lab/lab11/lab11.scm
@@ -1,15 +1,25 @@
 (define (if-program condition if-true if-false)
-  'YOUR-CODE-HERE)
+  (list 'if condition if-true if-false)
+)
 
 (define (square n) (* n n))
 
-(define (pow-expr base exp) 'YOUR-CODE-HERE)
+; Like Higher Order Function
+(define (pow-expr base exp) 
+  (if (even? exp)
+    (if (= exp 0)
+      1
+      `(square ,(pow-expr base (/ exp 2))) ; list('square (pow-expr base (/ exp 2)))
+    )
+    `(* ,base ,(pow-expr base (- exp 1))) ; (list '* base (pow-expr base (- exp 1)))
+  )
+)
 
 (define-macro (repeat n expr)
-  `(repeated-call ,n ___))
+  `(repeated-call ,n (lambda () ,expr)))
 
 ; Call zero-argument procedure f n times and return the final result.
 (define (repeated-call n f)
   (if (= n 1)
-      ___
-      (begin ___ ___)))
+      (f)
+      (begin (f) (repeated-call (- n 1) f))))
